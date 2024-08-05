f656b869d2aed7edadcf82c1755c408fbc38bc9d
Neos <wael.monsri@proton.me>
2024-08-05 07:07:56 +0100

    Initial status non-ready deploy

---
+++ query_data.py
@@ -0,0 +1,61 @@
+import argparse
+from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+
+from get_embedding_function import get_embedding_function
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+You are an assistant who will answer questions 
+depending on the language used in the question 
+if you don't know the answer you can refer to your own data to answer from 
+if you referred to your own data you should mention to the user that the response 
+doesn't exist in his document and provided a possible answer 
+if you referred to your own knowledge and you are unsure of the correctness of the information
+you should inform the user as well . 
+you should be polite as possible .
+You would Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
+
+
+def query_rag(query_text: str):
+    # Prepare the DB.
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    # print(prompt)
+
+    model = Ollama(model="llama3")
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+    return response_text
+
+
+if __name__ == "__main__":
+    main()
